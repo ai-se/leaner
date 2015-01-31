e17981087d0c49bb512ff1a0dc19894a76e7939e
Tim Menzies <menzies.tim@gmail.com>
2015-01-31 22:51:00 +0000

    stuff

--- src/ranker.py
+++ src/ranker.py
@@ -12,7 +12,7 @@ def g(x): return ('%g' % x)
 def _rangeLib():
   def show(n,rule):
     print(n,rule)
-    print(n,sorted(map(lambda z:z.id,rule.rows)))  
+    #print(n,sorted(map(lambda z:z.id,rule.rows)))  
   t=housing.housing() 
   b4=Counts(map(lambda l:l[-1],t.data))
   r=[]
@@ -26,16 +26,18 @@ def _rangeLib():
       print(one.attr, 
         one.x.lo, one.x.hi,g(one.y.mu),g(one.y.sd),g(b4.mu),g(b4.sd()),sep=",")
   #exit()   
-  rules =map(lambda r:Rule([r],r.rows))
-  for _ in range(20):
-      print("")
+  rules =map(lambda z:Rule([z],z.rows),r)
+  for _ in range(200): 
       rule1  = random.choice(rules)
       rule2  = random.choice(rules) 
-      show(1,rule1)
-      show(2,rule2)
+      
       rule3 = rule1+rule2
       if rule3:
+        print("")
+        show(1,rule1)
+        show(2,rule2)
         show(3,rule3)
+        rules += [rule3]
       
 if __name__ == "__main__":
     _rangeLib()
