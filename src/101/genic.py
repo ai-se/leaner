9d143878dc0b177e8770624ce45e45aa0347e70d
Tim Menzies <tim@menzies.us>
2015-02-19 23:19:09 -0500

    stuff

--- src/101/genic.py
+++ src/101/genic.py
@@ -44,11 +44,60 @@ sys.dont_write_bytecode=True
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""
+"""# Lib.py
 
-# Lib.py
+This file defines a basic set of tools
+to be used by  a
+"software scientist".   
 
-This file defines a workbench for the software scientist. 
+The world we live
+in today is much more a man-made,1 or
+artificial, world than it is a natural
+world. Almost every element in our
+environment shows evidence of human
+artifice.
+
+Because of its abstract character and
+its symbol manipulating generality, the
+digital computer has greatly extended
+the range of systems whose behavior can
+be imitated. Generally we now call the
+imitation "simulation," and we try to
+understand the imitated system by
+testing the simulation in a variety of
+simulated, or imitated, environments.
+Artificial systems and adaptive systems
+have properties that make them
+particularly susceptible to simulation
+via simplified models.
+
+A frequently asked question is how can a
+simulation ever tell us anything that we
+do not already know?  The obvious point
+is that, even when we have correct
+premises, it may be very difficult to
+discover what they imply. All correct
+reasoning is a grand system of
+tautologies, but only God can make
+direct use of that fact. The rest of us
+must painstakingly and fallibly tease
+out the consequences of our assumptions.
+
+In science and engineering the study of
+"systems" is an increasingly popular
+activity. Its popularity is more a
+response to a pressing need for
+synthesizing and analyzing complexity
+than it is to any large development of a
+body of knowledge and technique for
+dealing with complexity. If this
+popularity is to be more than a fad,
+necessity will have to mother invention
+and provide substance to go with the
+name.
+
+
+
 
 ## Principles
 
